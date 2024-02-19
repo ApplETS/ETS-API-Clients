722921f347b3732c394cee3d61f10d4874d4b5a0
MysticFragilist <mont.samuel@outlook.com>
2024-02-18 23:39:24 -0500

    add endpoint to get events

---
+++ lib/src/hello_api_client_implementation.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/io_client.dart';
+
+import 'constants/http_exception.dart';
+import 'constants/urls.dart';
+
+import 'hello_api_client.dart';
+
+import 'models/news.dart';
+import 'package:http/http.dart' as http;
+
+/// A Wrapper for all calls to Hello API.
+class HelloAPIClient implements IHelloAPIClient {
+  static const String tag = "HelloApi";
+  static const String tagError = "$tag - Error";
+
+  final http.Client _httpClient;
+
+  HelloAPIClient({http.Client? client})
+      : _httpClient = client ?? IOClient(HttpClient());
+
+  /// Call the Hello API to get the news
+  /// [startDate] The start date of the news (optional)
+  /// [endDate] The end date of the news (optional)
+  /// [tags] The tags of the news (optional)
+  /// [activityAreas] The activity areas of the news (optional)
+  /// [pageNumber] The page number (default: 1)
+  /// [pageSize] The page size (default: 10)
+  @override
+  Future<List<News>> getEvents(
+      {DateTime? startDate,
+      DateTime? endDate,
+      List<String>? tags,
+      List<String>? activityAreas,
+      int pageNumber = 1,
+      int pageSize = 10}) async {
+    final query = {
+      'startDate': startDate,
+      'endDate': endDate,
+      'tags': tags,
+      'activityAreas': activityAreas,
+      'pageNumber': pageNumber.toString(),
+      'pageSize': pageSize.toString(),
+    };
+    final uri = Uri.http(Urls.helloNewsAPI, '/api/news', query);
+    final response = await _httpClient.get(uri);
+
+    // Log the http error and throw a exception
+    if (response.statusCode != 200) {
+      throw HttpException(
+          message: response.body, prefix: tagError, code: response.statusCode);
+    }
+
+    final body = jsonDecode(response.body) as Map<String, dynamic>;
+    final news = body['data'] as List<dynamic>;
+    return news.map((e) => News.fromJson(e as Map<String, dynamic>)).toList();
+  }
+}
---
+++ test/hello_api_client_test.dart
@@ -0,0 +1,126 @@
+import 'dart:convert';
+import 'package:ets_api_clients/src/constants/urls.dart';
+import 'package:ets_api_clients/src/models/news.dart';
+import 'package:ets_api_clients/src/models/news_user.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+import 'package:ets_api_clients/models.dart';
+import 'package:ets_api_clients/src/constants/http_exception.dart';
+import 'package:ets_api_clients/clients.dart';
+import 'package:test/test.dart';
+
+import 'mocks/http_client_mock_helper.dart';
+
+void main() {
+  late HelloAPIClient service;
+  late MockClient mockClient;
+
+  group('HelloApi - ', () {
+    setUp(() {
+      // default response stub
+      mockClient = MockClient((request) => Future.value(Response("", 200)));
+
+      service = HelloAPIClient(client: mockClient);
+    });
+
+    tearDown(() {
+      mockClient.close();
+    });
+
+    group('getEvents - ', () {
+      test('empty data', () async {
+        final query = {
+          'startDate': null,
+          'endDate': null,
+          'tags': null,
+          'activityAreas': null,
+          'pageNumber': 1.toString(),
+          'pageSize': 10.toString(),
+        };
+        final uri = Uri.http(Urls.helloNewsAPI, '/api/news', query);
+        mockClient = HttpClientMockHelper.stubJsonGet(uri.toString(), {
+          'data': [],
+          'pageNumber': 1,
+          'pageSize': 10,
+          'totalPages': 1,
+          'totalRecords': 0
+        });
+        service = buildService(mockClient);
+
+        final result = await service.getEvents();
+
+        expect(result, isA<List<News>>());
+        expect(result.length, 0);
+      });
+
+      test('one news', () async {
+        final news = News(
+            id: "402e711c-0f72-4aab-9684-31f1956c1da1",
+            title: "title",
+            content: "content",
+            imageUrl: "imageUrl",
+            state: 1,
+            publicationDate: DateTime.now(),
+            eventStartDate: DateTime.now().add(const Duration(days: 4)),
+            eventEndDate: DateTime.now().add(const Duration(days: 4, hours: 2)),
+            createdAt: DateTime.now().subtract(const Duration(days: 4)),
+            updatedAt: DateTime.now().subtract(const Duration(days: 4)),
+            moderator: NewsUser(
+              id: "3783f79f-da78-4486-8a5a-7b855b856033",
+              name: "name",
+              email: "email",
+              type: "moderator",
+              createdAt: DateTime.now().subtract(const Duration(days: 30)),
+              updatedAt: DateTime.now().subtract(const Duration(days: 30)),
+            ),
+            organizer: NewsUser(
+              id: "3a5cb049-67cf-428e-b98f-ef29fb633e0d",
+              name: "name2",
+              email: "email2",
+              type: "organizer",
+              createdAt: DateTime.now().subtract(const Duration(days: 30)),
+              updatedAt: DateTime.now().subtract(const Duration(days: 30)),
+            ));
+
+        final query = {
+          'startDate': null,
+          'endDate': null,
+          'tags': null,
+          'activityAreas': null,
+          'pageNumber': 1.toString(),
+          'pageSize': 10.toString(),
+        };
+        final uri = Uri.http(Urls.helloNewsAPI, '/api/news', query);
+        mockClient = HttpClientMockHelper.stubJsonGet(uri.toString(), {
+          'data': [news],
+          'pageNumber': 1,
+          'pageSize': 10,
+          'totalPages': 1,
+          'totalRecords': 1
+        });
+        service = buildService(mockClient);
+
+        final result = await service.getEvents();
+
+        expect(result, isA<List<News>>());
+        expect(result.length, 1);
+        expect(result[0].id, "402e711c-0f72-4aab-9684-31f1956c1da1");
+      });
+
+      test('any other errors for now', () async {
+        const int statusCode = 500;
+        const String message = "An error has occurred.";
+
+        mockClient = HttpClientMockHelper.stubJsonPost(
+            Urls.helloNewsAPI, {"Message": message}, statusCode);
+        service = buildService(mockClient);
+
+        expect(service.getEvents(), throwsA(isA<HttpException>()));
+      });
+    });
+  });
+}
+
+HelloAPIClient buildService(MockClient client) =>
+    HelloAPIClient(client: client);
